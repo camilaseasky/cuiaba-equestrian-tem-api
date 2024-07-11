66593978ee3a0f871027b5a0396c3a783ffac5b7
camila.barbosa <camila.barbosa@totemti.com.br>
2024-07-11 13:00:25 -0400

    commit inicial

---
+++ src/main/java/br/com/cuiabaequestrianteam/cuiaba_equestrian_team/CuiabaEquestrianTeamApplication.java
@@ -0,0 +1,13 @@
+package br.com.cuiabaequestrianteam.cuiaba_equestrian_team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuiabaEquestrianTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuiabaEquestrianTeamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cuiabaequestrianteam/cuiaba_equestrian_team/CuiabaEquestrianTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cuiabaequestrianteam.cuiaba_equestrian_team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuiabaEquestrianTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
